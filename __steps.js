5c7ccee4a1c631eab919846b3536c455d99f7f4b
sajid365-sr <sajidsorker86@gmail.com>
2022-11-07 04:14:55 +0600

    getting data from client side using app.post method,__steps file added

---
+++ __steps.js
@@ -0,0 +1,11 @@
+/* 
+POST
+
+1. Create Post API
+2. Use fetch with Method post
+3. Add Header to the fetch > 'content-type': "application/json"
+4. Add body to the fetch to send data
+5. make sure to use JSON.stringify to send data
+6. use express.json as middleware to get the req data otherwise it will be undefiled.
+
+*/
--- index.js
+++ index.js
@@ -2,4 +2,33 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
+
+app.get('/', (req, res) =>{
+    res.send('Simple Node Server Running');
+});
+
+app.use(cors());
+app.use(express.json());
+
+
+const users = [
+    {id:1, name:'player1', email:'player1@gmail.com'},
+    {id:2, name:'player2', email:'player2@gmail.com'},
+    {id:3, name:'player3', email:'player3@gmail.com'},
+    {id:4, name:'player4', email:'player4@gmail.com'},
+]
+
+app.get('/users', (req,res) =>{
+    res.send(users);
+})
+
+app.post('/users', (req, res) =>{
+    console.log("Post API Called")
+    console.log(req.body)
+})
+
+app.listen(port, () =>{
+    console.log(`Simple node server running on port: ${port}`);
+});
